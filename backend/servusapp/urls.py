352f17babd4c79f87e2b0d64129e9bc839328bbd
kevinngonyc <kevinngonyc@gmail.com>
2018-01-05 18:20:52 -0500

    added GET api endpoints for all models

--- backend/servusapp/urls.py
+++ backend/servusapp/urls.py
@@ -5,8 +5,18 @@ from rest_framework.authtoken import views as rest_framework_views
 
 urlpatterns = [
     path('users/', views.users, name='json_users'),
-    path('user/<int:user_id>/', views.user, name='json_user'),
+    path('users/<int:user_id>/', views.user, name='json_user'),
     path('login/', views.login, name='json_login'),
     path('me/', views.me, name='json_me'),
     path('login_auth/', rest_framework_views.obtain_auth_token, name='get_auth_token'),
+    path('categories/', views.service_categories, name='json_service_categories'),
+    path('services/', views.services, name='json_services'),
+    path('services/<int:service_id>/', views.service, name='json_service'),
+    path('service-images/<int:service_id>/', views.service_images, name='json_service_images'),
+    path('service-prices/<int:service_id>/', views.service_prices, name='json_service_prices'),
+    path('service-times/<int:service_id>/', views.service_times, name='json_service_times'),
+    path('service-reviews/<int:service_id>/', views.service_reviews, name='json_service_reviews'),
+    path('reviews/', views.reviews, name='json_reviews'),
+    path('transactions/', views.transactions, name='json_transactions'),
+    path('messages/', views.messages, name='json_tmessages'),
 ]
--- backend/servusapp/views.py
+++ backend/servusapp/views.py
@@ -20,29 +20,82 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 def users(request):
-    qstring_dict = QueryDict(query_string=request.META["QUERY_STRING"])
-    if qstring_dict:
-        u = User.objects.filter(**delist_dict(qstring_dict))
-    else:
-        u = User.objects.all()
-    users = serializers.serialize("json", u)
-    return HttpResponse(users, content_type="application/json")
-
+	if request.method == 'GET':
+		return process_qstring(request, User.objects)
+	elif request.method == 'POST':
+		#To be implemented
+		#Perhaps all post methods can be implemented in process_qstring
+		return
 
 def user(request, user_id):
-    try:
-        u = User.objects.get(pk=user_id)
-        _user = serializers.serialize("json", [u, ])
-    except ObjectDoesNotExist:
-        raise Http404("User does not exist")
-    return HttpResponse(_user, content_type="application/json")
+	try:
+		_user = serializers.serialize("json", [User.objects.get(pk=user_id)])
+	except User.DoesNotExist:
+		raise Http404("User does not exist")
+	return HttpResponse(_user, content_type="application/json")
 
+
+def service_categories(request):
+	return process_qstring(request, Service_Category.objects)
+
+def services(request):
+	return process_qstring(request, Service.objects)
+
+def service(request, service_id):
+	try:
+		service = Service.objects.select_related('provider').get(pk=service_id)
+		service_json = json.loads(serializers.serialize("json", [service]))[0]
+		provider_json = json.loads(serializers.serialize("json", [service.provider]))[0]
+		service_json['fields']['provider']=provider_json
+	except Service.DoesNotExist:
+		raise Http404("Service does not exist")
+	return HttpResponse(json.dumps(service_json), content_type="application/json")
+
+def service_images(request, service_id):
+	return process_qstring(request, Service_Image.objects.filter(service=service_id))
+
+def service_prices(request, service_id):
+	return process_qstring(request, Service_Price.objects.filter(service=service_id))
+
+def service_times(request, service_id):
+	return process_qstring(request, Service_Time.objects.filter(service=service_id))
+
+def service_reviews(request, service_id):
+	return process_qstring(request, Review.objects.filter(service=service_id))
+
+
+def reviews(request):
+	return process_qstring(request, Review.objects)
+
+def messages(request):
+	return process_qstring(request, Message.objects)
+
+def transactions(request):
+	return process_qstring(request, Transaction.objects)
+
+def disputes(request):
+	return process_qstring(request, Dispute.objects)
+
+def bookings(request):
+	return process_qstring(request, Dispute.objects)
+
+
+def process_qstring(request, model):
+	qstring_dict = QueryDict(query_string=request.META["QUERY_STRING"])
+	if (qstring_dict):
+		#If search, sort, etc.
+		#Else
+		obj = model.filter(**delist_dict(qstring_dict))
+	else:
+		obj = model.all()
+	json_obj = serializers.serialize("json", obj)
+	return HttpResponse(json_obj, content_type="application/json")
 
 def delist_dict(qstring_dict):
-    delisted_dict = {}
-    for key in qstring_dict:
-        delisted_dict[key] = qstring_dict[key]
-    return delisted_dict
+	delisted_dict = {}
+	for key in qstring_dict:
+		delisted_dict[key] = qstring_dict[key]
+	return delisted_dict
 
 def login(request):
     try:
@@ -63,7 +116,7 @@ def register(user):
     print("user created")
     print(user)
     u = User(
-        first_name=user["givenName"], 
+        first_name=user["givenName"],
         last_name=user["familyName"],
         username=user["id"],
         password=make_password(user["id"]),
